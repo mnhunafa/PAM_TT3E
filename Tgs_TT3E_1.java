483d88f630e26daa7c0f0fbcb571c221691712fc
mnhunafa <43032612+mnhunafa@users.noreply.github.com>
2018-09-07 10:57:39 +0700

    Add files via upload

---
+++ Tgs_TT3E_1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tgs_tt3e_1;
+
+/**
+ *
+ * @author Meila Nadya Hunafa
+ */
+public class Tgs_TT3E_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int jumlah = 5;
+        for (int m=1; m<=jumlah; m++){
+            for (int n=1; n<=m; n++){
+            System.out.print("*");   
+            }
+            System.out.println();
+        }
+    }
+    
+}
